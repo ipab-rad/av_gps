625f383ee28bf1325c0d92021340d69ae6dbd946
Hector Cruz <37127013+hect95@users.noreply.github.com>
2024-04-30 15:11:06 +0100

    Porting code from ipab-rad/novatel_gps_driver (#1)
    * Adding pre-commit files
    
    * Porting code from ipab-rad/novategl_gps_driver
    
    - The novatel ROS 2 driver is now installed from
      the apt repository rather than from source
    - Preserving same the run.sh and dev.sh functionality
    
    * Applying pre-commit fixes
    
    * Removing unnecessary docker volume mounts
    
    * Fixing typo
    
    * Fix pre-commit config to follow other repos
    
    * Remove Dockerfile deps only needed for compilation
    
    * Add CHANGELOG and prepare pkg for first release
    
    * Add .github workflow ahead of release
    
    * Fix README as per comments
    
    * Updating README format
    
    ---------
    
    Co-authored-by: Alejandro Bordallo <alex.bordallo@gmail.com>

---
+++ dev.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# ----------------------------------------------------------------
+# Build docker dev stage and add local code for live development
+# ----------------------------------------------------------------
+
+# Build docker image up to dev stage
+DOCKER_BUILDKIT=1 docker build \
+    -t av_gps:latest \
+    -f Dockerfile --target dev .
+
+# Run docker image with local code volumes for development
+docker run -it --rm --net host --privileged \
+    -v /dev/shm:/dev/shm \
+    -v ./av_novatel_launch:/opt/ros_ws/src/av_novatel_launch \
+    av_gps:latest
