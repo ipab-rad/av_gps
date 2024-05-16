1c7cc24a11cd2ea31464f8a295c77a02c9139721
hect95 <hector_cruz95@live.com>
2024-05-16 13:06:19 +0100

    Avoid overriding dev and runtime images

--- dev.sh
+++ dev.sh
@@ -5,11 +5,11 @@
 
 # Build docker image up to dev stage
 DOCKER_BUILDKIT=1 docker build \
-    -t av_gps:latest \
+    -t av_gps:latest-dev \
     -f Dockerfile --target dev .
 
 # Run docker image with local code volumes for development
 docker run -it --rm --net host --privileged \
     -v /dev/shm:/dev/shm \
     -v ./av_novatel_launch:/opt/ros_ws/src/av_novatel_launch \
-    av_gps:latest
+    av_gps:latest-dev
